69576f15fc7b075b0d8c35081d00e9afb21d91b8
Nutanix Circle CI Release Agent <api-packaging@nutanix.com>
2022-05-19 04:06:34 +0000

    hello: changes for v0.0.1-rc2

---
+++ nutanix-hello-world.go
@@ -0,0 +1,12 @@
+package nutanix_hello_world
+
+//import (
+    //"fmt"
+    //"os"
+//)
+
+func HelloWorld() string {
+    //fmt.Println("hello nutanix world")
+	return "hello nutanix world"
+	//os.Exit(0)
+}
