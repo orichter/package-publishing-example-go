868c800f4094c990e573396f9dd6d0f2126bda2c
Owen Richter <owen@nutanix.com>
2022-07-08 14:34:03 +0000

    add python publishing

--- services/deploy/deploy-python.sh
+++ services/deploy/deploy-python.sh
@@ -1,24 +1,50 @@
 #!/bin/bash
-#shellcheck disable=SC1091
 #export TERM=ansi
 export TERM=xterm-color
+PROJECT_ROOT=${PROJECT_ROOT:-"/home/circleci/project"}
+EXIT_STATUS=0
+#shellcheck disable=SC1091
+#shellcheck disable=SC1090
+source "${PROJECT_ROOT}"/services/deploy/release-utils.source
 
-function debug {
-  env |grep -v PASSWORD
-  pwd
-  ls -lah
-  ls -lah ./services
-  ls -lah ./services/deploy
+#shellcheck disable=SC1091
+#shellcheck disable=SC1090
+source "${PROJECT_ROOT}"/release-config.source
+export VERSION="${DEPLOY_TO_TAG}"
+INFO "Deploying npm packages using Release Params:"
+cat "${PROJECT_ROOT}"/release-config.source
+INFO "Version: ${VERSION}"
+echo
+
+function main {
+  #deploy-to-stage-internal
+  deploy-to-stage
+  #deploy-to-github-prod
+  #deploy-to-prod
 }
 
-source ./release-config.source
-echo "Deploying python packages with Release Params:"
-cat ./release-config.source
-echo
-pushd ./pypi/packaging_tutorial/ || exit 1
-rm ./dist/*
-python3 -m pip install --upgrade build
-python3 -m build
-python3 -m pip install --upgrade twine
-python3 -m twine upload --repository testpypi --username __token__ --password "${PASSWORD_PUBLISH_TESTPYPI}" dist/*
-popd || exit 1
+function deploy-to-stage {
+  pushd "${PROJECT_ROOT}"/pip-release-verify || exit 1
+
+  cp -rf package stage-package-internal
+
+  pushd stage-package-internal || exit 1
+
+  rm ./dist/*
+  python3 -m pip install --upgrade build
+  python3 -m build
+  python3 -m pip install --upgrade twine
+  python3 -m twine upload --repository testpypi --username __token__ --password "${PASSWORD_PUBLISH_TESTPYPI}" dist/*
+  popd || exit 1
+
+  popd || exit 1
+
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  main "$@"
+else
+  echo "This file is designed to be executed, not sourced."
+fi
+
+exit "${EXIT_STATUS}"
