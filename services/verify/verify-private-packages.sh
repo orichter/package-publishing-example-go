24d374a4fc9e0a7606a97f63c4154886e4163df1
Owen Richter <owen@nutanix.com>
2022-08-15 17:30:34 +0000

    refactor to iterate over namespaces

--- services/verify/verify-private-packages.sh
+++ services/verify/verify-private-packages.sh
@@ -26,11 +26,44 @@ function main {
   # It should be commented out for actual deployments.
   check-prerequisites
   #mvn-sample-release
+
+  rm -rf "${PROJECT_ROOT}"/verify/pip-release-verify
+  mkdir -p "${PROJECT_ROOT}"/verify/pip-release-verify
+  rm -rf "${PROJECT_ROOT}"/verify/npm-release-verify
+  mkdir -p "${PROJECT_ROOT}"/verify/npm-release-verify
+  rm -rf "${PROJECT_ROOT}"/verify/golang-release-verify
+  mkdir -p "${PROJECT_ROOT}"/verify/golang-release-verify
+  rm -rf "${PROJECT_ROOT}"/verify/maven-release-verify
+  mkdir -p "${PROJECT_ROOT}"/verify/maven-release-verify
+
+  # DEPLOY_FROM_TAG: "4.0.1-alpha.1"
+  # DEPLOY_TO_TAG="0.1.4-0-1-rc1"
+
+  NAMESPACES="vmm prism clustermgmt aiops iam storage"
+  # Iterate the string variable using for loop
+  for NAMESPACE in ${NAMESPACES}; do
+    echo "Verifying Namespace: ${NAMESPACE}"
+    # HACK: Crude deployment manifest which needs to be reworked.
+    if [ "${NAMESPACE}" = "storage" ] ; then
+      VERSION="4.0.1-alpha.2"
+      MVN_VERSION="4.0.1-alpha-1"
+    else
+      VERSION="${DEPLOY_FROM_TAG}"
+      MVN_VERSION="${DEPLOY_FROM_TAG}"
+    fi
+    pip-internal-release-verify "${NAMESPACE}" "${VERSION}"
+    npm-internal-release-verify "${NAMESPACE}" "${VERSION}"
+
+    # HACK: MVN Deployment Version is currently pinned.
+    #VERSION="4.0.0-alpha-1"
+    MVN_VERSION="${MVN_VERSION/4.0.1/4.0.0}"
+    MVN_VERSION=${MVN_VERSION/alpha./alpha-}
+    WARN "MVN Version rewritten to: ${MVN_VERSION}"
+    mvn-internal-release-verify "${NAMESPACE}" "${MVN_VERSION}"
+
+  done
   # HACK: golang currently can't be verified, so it is removed.
-  pip-internal-release-verify
-  npm-internal-release-verify
-  mvn-internal-release-verify
-  golang-internal-release-verify
+  #golang-internal-release-verify
 }
 
 function check-prerequisites {
@@ -48,7 +81,7 @@ function check-prerequisites {
 
 function golang-internal-release-verify {
   export VERSION="${DEPLOY_FROM_TAG}"
-  #HACK: This should be named golang-client for consistency
+  # HACK: This should be named golang-client for consistency
   export PACKAGE_NAME="${NAMESPACE}"-go-client
 
   sudo curl -O https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
@@ -57,8 +90,6 @@ function golang-internal-release-verify {
   export PATH=$PATH:/usr/local/go/bin
   go version
 
-  rm -rf "${PROJECT_ROOT}"/verify/golang-release-verify
-  mkdir -p "${PROJECT_ROOT}"/verify/golang-release-verify
   pushd "${PROJECT_ROOT}"/verify/golang-release-verify || exit 1
   
   export REPO_NAME=ntnx-api-golang-sdk-external
@@ -102,22 +133,18 @@ function golang-internal-release-verify {
 }
 
 function mvn-internal-release-verify {
-  export VERSION="${DEPLOY_FROM_TAG}"
-  #HACK Version is currently pinned.
-  VERSION="4.0.0-alpha-1"
+  NAMESPACE=$1
+  VERSION=$2
   export PACKAGE_NAME="${NAMESPACE}"-java-client
-  #HACK: Other package versions should be made consistent with MVN_VERSION
+  # HACK: Other package versions should be made consistent with MVN_VERSION
   # If this is done, this hack can be removed.
   export MVN_VERSION=${VERSION/alpha./alpha-}
 
-  rm -rf "${PROJECT_ROOT}"/verify/maven-release-verify
-  mkdir -p "${PROJECT_ROOT}"/verify/maven-release-verify
-
   pushd "${PROJECT_ROOT}"/verify/maven-release-verify || exit 1
 
-  mvn archetype:generate -q -DgroupId=com.nutanix.test.sdk -DartifactId=test-sdk-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+  mvn archetype:generate -q -DgroupId=com.nutanix.test.sdk -DartifactId=test-"${NAMESPACE}"-sdk-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
 
-  pushd test-sdk-app || exit 1
+  pushd test-"${NAMESPACE}"-sdk-app || exit 1
   mv pom.xml pom.xml.old
   cp "${PROJECT_ROOT}"/services/deploy/mvn/pom.template ./pom.xml
   cp "${PROJECT_ROOT}"/services/deploy/mvn/settings.xml .
@@ -126,14 +153,14 @@ function mvn-internal-release-verify {
   #cat pom.xml
 
   if mvn package -q --settings settings.xml -DdownloadSources=true -DdownloadJavadocs=true ; then
-    PASS "Successfully Downloaded Maven Package using pom:"
+    PASS "Successfully Downloaded Maven Package: ${PACKAGE_NAME} using pom:"
     cat pom.xml
     mvn dependency:sources dependency:resolve -Dclassifier=javadoc -q -s settings.xml
     mvn dependency:sources -Dsilent=true -q -s settings.xml
-    cp -rf ~/.m2/repository/com/nutanix/api/"${PACKAGE_NAME}"/"${MVN_VERSION}"/ "${PROJECT_ROOT}"/verify/maven-release-verify/package
+    cp -rf "${HOME}"/.m2/repository/com/nutanix/api/"${PACKAGE_NAME}"/"${MVN_VERSION}"/ "${PROJECT_ROOT}"/verify/maven-release-verify/package-"${NAMESPACE}"
     INFO "MVN Package Contents:"
-    ls -lah "${PROJECT_ROOT}"/verify/maven-release-verify/package
-    pushd "${PROJECT_ROOT}"/verify/maven-release-verify/package || exit 1
+    ls -lah "${PROJECT_ROOT}"/verify/maven-release-verify/package-"${NAMESPACE}"
+    pushd "${PROJECT_ROOT}"/verify/maven-release-verify/package-"${NAMESPACE}" || exit 1
     #mvn dependency:sources dependency:resolve -Dclassifier=javadoc -q -s settings.xml
     #mvn dependency:sources -Dsilent=true -q -s settings.xml
     #ls -lah "${PROJECT_ROOT}"/verify/maven-release-verify/package
@@ -141,7 +168,7 @@ function mvn-internal-release-verify {
     popd || exit 1
     echo
   else
-    ERROR "Failed to Download Maven Package"
+    ERROR "Failed to Download Maven Package ${PACKAGE_NAME}"
     export EXIT_STATUS=1
     debug
   fi
@@ -180,18 +207,17 @@ function mvn-internal-release-verify {
 }
 
 function npm-internal-release-verify {
-  export VERSION="${DEPLOY_FROM_TAG}"
+  NAMESPACE=$1
+  VERSION=$2
   export PACKAGE_NAME="${NAMESPACE}"-js-client
   export SCOPE=nutanix-core
 
-  rm -rf "${PROJECT_ROOT}"/verify/npm-release-verify
-  mkdir -p "${PROJECT_ROOT}"/verify/npm-release-verify
   pushd "${PROJECT_ROOT}"/verify/npm-release-verify || exit 1
   
   echo "${INTERNAL_NPM_PULL_CREDENTIALS}" > "${HOME}/.npmrc"
 
   if npm pack @nutanix-core/"${PACKAGE_NAME}"@"${VERSION}" ; then
-    PASS "Successfully Downloaded NPM Package"
+    PASS "Successfully Downloaded NPM Package: ${PACKAGE_NAME}"
     INFO @"${SCOPE}"/"${PACKAGE_NAME}"@"${VERSION}"
   else
     ERROR "Failed to Download NPM Package"
@@ -199,17 +225,18 @@ function npm-internal-release-verify {
     export EXIT_STATUS=1
   fi
 
-  ls -lah
+  #ls -lah
   tar -zxf "${SCOPE}"-"${PACKAGE_NAME}"-"${VERSION}".tgz
   # This should be untarred into package which will be used in deploy-npm.sh for actual deployments
 
-  cp -rf package audit
-  pushd package || exit 1
+  mv -f package package-"${NAMESPACE}"
+  cp -rf package-"${NAMESPACE}" audit-"${NAMESPACE}"
+  pushd package-"${NAMESPACE}" || exit 1
   # This is necessary so the blackduck scan doesn't complain.
   npm i > /dev/null
   popd || exit 1
 
-  pushd audit || exit 1
+  pushd audit-"${NAMESPACE}" || exit 1
   VULNERABILITIES_COUNT=$(npm i --package-lock-only|grep vulnerabilities)
 
   CRITICAL_COUNT=$(echo "${VULNERABILITIES_COUNT}" | grep -oP '\w+(?= critical)')
@@ -228,40 +255,39 @@ function npm-internal-release-verify {
 }
 
 function pip-internal-release-verify {
-  export VERSION="${DEPLOY_FROM_TAG}"
+  NAMESPACE=$1
+  VERSION=$2
   PACKAGE_DIR="${NAMESPACE}"
   export PACKAGE_NAME="ntnx_${NAMESPACE}_py_client"
   export PACKAGE_NAME="${PACKAGE_NAME/_/-}"
 
-  rm -rf "${PROJECT_ROOT}"/verify/pip-release-verify
-  mkdir -p "${PROJECT_ROOT}"/verify/pip-release-verify
   pushd "${PROJECT_ROOT}"/verify/pip-release-verify || exit 1
   
-  sudo apt install python3-pip
-  /home/circleci/.pyenv/versions/3.8.5/bin/python3.8 -m pip install --upgrade pip
+  sudo apt install python3-pip > /dev/null
+  /home/circleci/.pyenv/versions/3.8.5/bin/python3.8 -m pip install --upgrade pip > /dev/null
 
   #PACKAGE_PATH="git+ssh://git@github.com/nutanix-core/ntnx-api-python-sdk-external.git@${TMP_VERSION}#subdirectory=${PACKAGE_DIR}&egg=${PACKAGE_NAME}"
   PACKAGE_PATH="git+ssh://git@github.com/nutanix-core/ntnx-api-python-sdk-external.git#subdirectory=${PACKAGE_DIR}&egg=${PACKAGE_NAME}"
 
   if pip3 install -e "${PACKAGE_PATH}" ; then
-    PASS "Successfully Downloaded Python Package"
+    PASS "Successfully Downloaded Python Package: ${PACKAGE_NAME}"
     INFO "${PACKAGE_PATH}"
   else
-    ERROR "Failed to Download Python Package"
+    ERROR "Failed to Download Python Package: ${PACKAGE_NAME}"
     INFO "${PACKAGE_PATH}"
     export EXIT_STATUS=1
   fi
 
   #cp -rf src/"${PACKAGE_NAME}"/"${PACKAGE_DIR}" ./package
-  ls -lah src/
-  echo
-  ls -lah src/"${PACKAGE_NAME}"
-  echo
-  ls -lah src/"${PACKAGE_DIR}"
-  echo
-  ls -lah src/"${PACKAGE_NAME}"/"${PACKAGE_DIR}"
-  echo
-  ls -lah src/"${PACKAGE_DIR}"/"${PACKAGE_NAME}"
+  #ls -lah src/
+  #echo
+  #ls -lah src/"${PACKAGE_NAME}"
+  #echo
+  #ls -lah src/"${PACKAGE_DIR}"
+  #echo
+  #ls -lah src/"${PACKAGE_NAME}"/"${PACKAGE_DIR}"
+  #echo
+  #ls -lah src/"${PACKAGE_DIR}"/"${PACKAGE_NAME}"
 
   popd || exit 1
 
