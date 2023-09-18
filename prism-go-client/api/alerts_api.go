52d75628ac78cec990e2eba046686645a2033c68
Nutanix SDK Publishing <sdk@nutanix.com>
2023-09-18 20:55:34 +0000

    Namespace: prism Version: v4.0.3-alpha.2

--- prism-go-client/api/alerts_api.go
+++ prism-go-client/api/alerts_api.go
@@ -4,14 +4,15 @@ package api
 import (
 	"encoding/json"
 	"github.com/orichter/package-publishing-example-go/prism-go-client/v4/client"
-	import1 "github.com/orichter/package-publishing-example-go/prism-go-client/v4/models/prism/v4/alerts"
+	import1 "github.com/orichter/package-publishing-example-go/prism-go-client/v4/models/prism/v4/serviceability"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
 type AlertsApi struct {
-	ApiClient *client.ApiClient
+	ApiClient     *client.ApiClient
+	headersToSkip map[string]bool
 }
 
 func NewAlertsApi(apiClient *client.ApiClient) *AlertsApi {
@@ -22,26 +23,24 @@ func NewAlertsApi(apiClient *client.ApiClient) *AlertsApi {
 	a := &AlertsApi{
 		ApiClient: apiClient,
 	}
+
+	headers := []string{"authorization", "cookie", "ntnx-request-id", "host", "user-agent"}
+	a.headersToSkip = make(map[string]bool)
+	for _, header := range headers {
+		a.headersToSkip[header] = true
+	}
+
 	return a
 }
 
-/**
-  Get alert details for a given UUID.
-  Get alert details for a given UUID.
-
-  parameters:-
-  -> extId (string) (required) : UUID of the generated alert.
-  -> args (map[string]interface{}) (optional) : Additional Arguments
-
-  returns: (*prism.v4.alerts.AlertApiResponse, error)
-*/
+// Get alert details for a given UUID.
 func (api *AlertsApi) GetAlertById(extId *string, args ...map[string]interface{}) (*import1.AlertApiResponse, error) {
 	argMap := make(map[string]interface{})
 	if len(args) > 0 {
 		argMap = args[0]
 	}
 
-	uri := "/api/prism/v4.0.a1/alerts/{extId}"
+	uri := "/api/prism/v4.0.a2/serviceability/alerts/{extId}"
 
 	// verify the required parameter 'extId' is set
 	if nil == extId {
@@ -60,13 +59,18 @@ func (api *AlertsApi) GetAlertById(extId *string, args ...map[string]interface{}
 	// to determine the Accept header
 	accepts := []string{"application/json"}
 
-	// Header Params
-	if ifMatch, ifMatchOk := argMap["If-Match"].(string); ifMatchOk {
-		headerParams["If-Match"] = ifMatch
+	// Headers provided explicitly on operation takes precedence
+	for headerKey, value := range argMap {
+		// Skip platform generated headers
+		if !api.headersToSkip[strings.ToLower(headerKey)] {
+			if value != nil {
+				if headerValue, headerValueOk := value.(string); headerValueOk {
+					headerParams[headerKey] = headerValue
+				}
+			}
+		}
 	}
-	if ifNoneMatch, ifNoneMatchOk := argMap["If-None-Match"].(string); ifNoneMatchOk {
-		headerParams["If-None-Match"] = ifNoneMatch
-	}
+
 	authNames := []string{"basicAuthScheme"}
 
 	responseBody, err := api.ApiClient.CallApi(&uri, http.MethodGet, nil, queryParams, headerParams, formParams, accepts, contentTypes, authNames)
@@ -78,26 +82,14 @@ func (api *AlertsApi) GetAlertById(extId *string, args ...map[string]interface{}
 	return unmarshalledResp, err
 }
 
-/**
-  Get the list of alerts.
-  Get the list of alerts.
-
-  parameters:-
-  -> page_ (int) (optional) : A URL query parameter that specifies the page number of the result set.  Must be a positive integer between 0 and the maximum number of pages that are available for that resource. Any number out of this range will lead to no results being returned.
-  -> limit_ (int) (optional) : A URL query parameter that specifies the total number of records returned in the result set.  Must be a positive integer between 0 and 100. Any number out of this range will lead to a validation error. If the limit is not provided a default value of 50 records will be returned in the result set.
-  -> filter_ (string) (optional) : A URL query parameter that allows clients to filter a collection of resources. The expression specified with $filter is evaluated for each resource in the collection, and only items where the expression evaluates to true are included in the response. Expression specified with the $filter must conform to the OData V4.01 URL conventions. The filter can be applied on the following fields: - acknowledged - alertType - autoResolved - classifications - clusterUUID - extId - resolved - serviceName - severity - sourceEntity
-  -> orderby_ (string) (optional) : A URL query parameter that allows clients to specify the sort criteria for the returned list of objects. Resources can be sorted in ascending order using asc or descending order using desc. If asc or desc are not specified the resources will be sorted in ascending order by default. For example, 'orderby=templateName desc' would get all templates sorted by templateName in desc order. The orderby can be applied to the following fields: - creationTimestamp - lastUpdatedTimestamp
-  -> args (map[string]interface{}) (optional) : Additional Arguments
-
-  returns: (*prism.v4.alerts.AlertListApiResponse, error)
-*/
+// Get the list of alerts.
 func (api *AlertsApi) GetAlerts(page_ *int, limit_ *int, filter_ *string, orderby_ *string, args ...map[string]interface{}) (*import1.AlertListApiResponse, error) {
 	argMap := make(map[string]interface{})
 	if len(args) > 0 {
 		argMap = args[0]
 	}
 
-	uri := "/api/prism/v4.0.a1/alerts"
+	uri := "/api/prism/v4.0.a2/serviceability/alerts"
 
 	headerParams := make(map[string]string)
 	queryParams := url.Values{}
@@ -111,25 +103,33 @@ func (api *AlertsApi) GetAlerts(page_ *int, limit_ *int, filter_ *string, orderb
 
 	// Query Params
 	if page_ != nil {
+
 		queryParams.Add("$page", client.ParameterToString(*page_, ""))
 	}
 	if limit_ != nil {
+
 		queryParams.Add("$limit", client.ParameterToString(*limit_, ""))
 	}
 	if filter_ != nil {
+
 		queryParams.Add("$filter", client.ParameterToString(*filter_, ""))
 	}
 	if orderby_ != nil {
+
 		queryParams.Add("$orderby", client.ParameterToString(*orderby_, ""))
 	}
+	// Headers provided explicitly on operation takes precedence
+	for headerKey, value := range argMap {
+		// Skip platform generated headers
+		if !api.headersToSkip[strings.ToLower(headerKey)] {
+			if value != nil {
+				if headerValue, headerValueOk := value.(string); headerValueOk {
+					headerParams[headerKey] = headerValue
+				}
+			}
+		}
+	}
 
-	// Header Params
-	if ifMatch, ifMatchOk := argMap["If-Match"].(string); ifMatchOk {
-		headerParams["If-Match"] = ifMatch
-	}
-	if ifNoneMatch, ifNoneMatchOk := argMap["If-None-Match"].(string); ifNoneMatchOk {
-		headerParams["If-None-Match"] = ifNoneMatch
-	}
 	authNames := []string{"basicAuthScheme"}
 
 	responseBody, err := api.ApiClient.CallApi(&uri, http.MethodGet, nil, queryParams, headerParams, formParams, accepts, contentTypes, authNames)
